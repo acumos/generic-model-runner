a936bd6ed624efc575c38e74197d0f681c3314f0
Teyu <thsiung@att.com>
2017-12-05 00:01:01 -0500

    Initial Commit
    Change-Id: I68d7e33f3fa65627b47fc2d513617db9b013cc31
    Signed-off-by: Teyu <thsiung@att.com>

---
+++ src/main/java/org/acumos/modelrunner/Application.java
@@ -0,0 +1,71 @@
+/*-
+ * ===============LICENSE_START=======================================================
+ * Acumos
+ * ===================================================================================
+ * Copyright (C) 2017 AT&T Intellectual Property & Tech Mahindra. All rights reserved.
+ * ===================================================================================
+ * This Acumos software file is distributed by AT&T and Tech Mahindra
+ * under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ * This file is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ===============LICENSE_END=========================================================
+ */
+
+package org.acumos.modelrunner;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * H2O/Generic Model Runner Application starting point
+ * 
+ *
+ */
+@SpringBootApplication
+public class Application implements ApplicationContextAware {
+	private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
+	public static final String CONFIG_ENV_VAR_NAME = "SPRING_APPLICATION_JSON";
+
+	public static void main(String[] args) throws Exception {
+		final String springApplicationJson = System.getenv(CONFIG_ENV_VAR_NAME);
+		if (springApplicationJson != null && springApplicationJson.contains("{")) {
+			final ObjectMapper mapper = new ObjectMapper();
+			// ensure it's valid
+			mapper.readTree(springApplicationJson);
+			logger.info("main: successfully parsed configuration from environment {}", CONFIG_ENV_VAR_NAME);
+		} else {
+			logger.warn("main: no configuration found in environment {}", CONFIG_ENV_VAR_NAME);
+		}
+		
+		ConfigurableApplicationContext cxt = SpringApplication.run(Application.class, args);
+		logger.info("main: context is {}", cxt);
+		
+
+		// Closing the context stops the application, so ignore
+		// the Sonar advice that the context must be closed here!
+	}
+
+	@Override
+	public void setApplicationContext(ApplicationContext context) {
+		((ConfigurableEnvironment) context.getEnvironment()).setActiveProfiles("src");
+	}
+
+
+}
+
